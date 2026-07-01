f3ff39f5f5c368e4d95c911acf7def6b2c8bb321
Johary Andriana <joharyandriana86@gmail.com>
2026-07-01 09:51:02 +0300

    Résolution des conflits et mise à jourfrontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,23 +13,52 @@ import Register from './pages/Register.jsx'
 import Account from './pages/Account.jsx'
 import Admin from './pages/Admin.jsx'
 
+import { useState, useEffect } from 'react'
+import { supabase } from './utils/supabase'
+
 export default function App() {
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    const getTodos = async () => {
+      const { data, error } = await supabase.from('todos').select()
+
+      if (error) {
+        console.log(error)
+      } else {
+        setTodos(data)
+      }
+    }
+
+    getTodos()
+  }, [])
+
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/tours" element={<Tours />} />
-        <Route path="/tours/:slug" element={<TourDetail />} />
-        <Route path="/stays" element={<Stays />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/quote" element={<Quote />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/account" element={<Account />} />
-        <Route path="/admin" element={<Admin />} />
-      </Route>
-    </Routes>
+    <>
+      {/* TEST SUPABASE */}
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.name}</li>
+        ))}
+      </ul>
+
+      {/* ROUTES */}
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/tours" element={<Tours />} />
+          <Route path="/tours/:slug" element={<TourDetail />} />
+          <Route path="/stays" element={<Stays />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/quote" element={<Quote />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/admin" element={<Admin />} />
+        </Route>
+      </Routes>
+    </>
   )
 }
